905ca5a0707b87524571d9d2d7fbdce31dcdbb82
Josh Burke <fealty@earthlink.net>
2012-02-12 02:05:12 -0800

    added checks

---
+++ Frost/Check.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Frost
+{
+	internal static class Check
+	{
+		public static bool IsFinite(float value)
+		{
+			return !Single.IsNaN(value) && !Single.IsInfinity(value);
+		}
+
+		public static bool IsNormalized(float value)
+		{
+			return value >= 0.0 && value <= 1.0;
+		}
+
+		public static bool IsDegrees(float value)
+		{
+			return value >= 0.0 && value <= 360.0;
+		}
+
+		public static bool IsPositive(float value)
+		{
+			return value >= 0.0 && IsFinite(value);
+		}
+	}
+}
--- Frost/Enumerations.cs
+++ Frost/Enumerations.cs
@@ -1,1 +1,35 @@
-﻿
+﻿namespace Frost
+{
+	public enum Alignment
+	{
+		Stretch,
+		Trailing,
+		Center,
+		Leading
+	}
+
+	public enum Axis
+	{
+		Both,
+		Horizontal,
+		Vertical
+	}
+
+	public enum LayoutDirection
+	{
+		LeftToRight,
+		RightToLeft
+	}
+
+	public enum ColorSpace
+	{
+		RGB,
+		HSV
+	}
+
+	public enum TransformMode
+	{
+		Multiply,
+		Replace
+	}
+}
