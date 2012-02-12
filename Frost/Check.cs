a2926347955af1804f90dfa0a26a01fd649b3228
Josh Burke <fealty@earthlink.net>
2012-02-12 04:44:13 -0800

    contracts

--- Frost/Check.cs
+++ Frost/Check.cs
@@ -4,26 +4,31 @@
 // See LICENSE for more information.
 
 using System;
+using System.Diagnostics.Contracts;
 
 namespace Frost
 {
 	public static class Check
 	{
+		[Pure]
 		public static bool IsFinite(float value)
 		{
 			return !Single.IsNaN(value) && !Single.IsInfinity(value);
 		}
 
+		[Pure]
 		public static bool IsNormalized(float value)
 		{
 			return value >= 0.0 && value <= 1.0;
 		}
 
+		[Pure]
 		public static bool IsDegrees(float value)
 		{
 			return value >= 0.0 && value <= 360.0;
 		}
 
+		[Pure]
 		public static bool IsPositive(float value)
 		{
 			return value >= 0.0 && IsFinite(value);
