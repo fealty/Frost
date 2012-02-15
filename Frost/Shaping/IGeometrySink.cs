073df855977324c44d696c3ff09674981cd81860
Josh Burke <fealty@earthlink.net>
2012-02-15 11:48:59 -0800

    igeometrysink

---
+++ Frost/Shaping/IGeometrySink.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) 2012, Joshua Burke
+// All rights reserved.
+// 
+// See LICENSE for more information.
+
+using System.Diagnostics.Contracts;
+
+namespace Frost.Shaping
+{
+	namespace Contracts
+	{
+		[ContractClassFor(typeof(IGeometrySink))]
+		internal abstract class IGeometrySinkContract : IGeometrySink
+		{
+			public abstract void Begin();
+			public abstract void End();
+			public abstract void Close();
+			public abstract void MoveTo(Point point);
+			public abstract void LineTo(Point point);
+			public abstract void QuadraticCurveTo(Point controlPoint, Point endPoint);
+
+			public abstract void BezierCurveTo(
+				Point controlPoint1, Point controlPoint2, Point controlPoint3);
+
+			public void ArcTo(Point tangentStart, Point tangentEnd, Size radius)
+			{
+				Contract.Requires(Check.IsPositive(radius.Width));
+				Contract.Requires(Check.IsPositive(radius.Height));
+			}
+		}
+	}
+
+	[ContractClass(typeof(Contracts.IGeometrySinkContract))]
+	public interface IGeometrySink
+	{
+		void Begin();
+		void End();
+		void Close();
+
+		void MoveTo(Point point);
+		void LineTo(Point point);
+
+		void QuadraticCurveTo(Point controlPoint, Point endPoint);
+
+		void BezierCurveTo(
+			Point controlPoint1, Point controlPoint2, Point controlPoint3);
+
+		void ArcTo(Point tangentStart, Point tangentEnd, Size radius);
+	}
+}
