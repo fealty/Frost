e78253278b4c7f1b00675e54ced860455d168aa1
Josh Burke <fealty@earthlink.net>
2012-02-17 23:42:04 -0800

    surface atlas

---
+++ Frost/Atlasing/ISurfaceAtlas.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+
+using Frost.Surfacing;
+
+namespace Frost.Atlasing
+{
+	namespace Contracts
+	{
+		[ContractClassFor(typeof(ISurfaceAtlas))]
+		internal abstract class ISurfaceAtlasContract : ISurfaceAtlas
+		{
+			public abstract bool InUse { get; }
+
+			public ISurface2D Surface2D
+			{
+				get
+				{
+					Contract.Ensures(Contract.Result<ISurface2D>() != null);
+
+					throw new NotSupportedException();
+				}
+			}
+
+			public Canvas Canvas
+			{
+				get
+				{
+					Contract.Ensures(Contract.Result<Canvas>() != null);
+
+					throw new NotSupportedException();
+				}
+			}
+
+			public Canvas AcquireRegion(Size size, object owner = null)
+			{
+				Contract.Requires(Check.IsPositive(size.Width));
+				Contract.Requires(Check.IsPositive(size.Height));
+
+				throw new NotSupportedException();
+			}
+
+			public abstract void Invalidate();
+		}
+	}
+
+	[ContractClass(typeof(Contracts.ISurfaceAtlasContract))]
+	public interface ISurfaceAtlas
+	{
+		bool InUse { get; }
+
+		ISurface2D Surface2D { get; }
+
+		Canvas Canvas { get; }
+
+		Canvas AcquireRegion(Size size, object owner = null);
+
+		void Invalidate();
+	}
+}
