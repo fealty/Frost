8fde9fc8046c2b58347939cd7291e1d039d03600
Josh Burke <fealty@earthlink.net>
2012-02-18 01:45:42 -0800

    effects

--- Frost/Device2D.cs
+++ Frost/Device2D.cs
@@ -4,21 +4,29 @@
 // See LICENSE for more information.
 
 using Frost.Diagnostics;
+using Frost.Effects;
 
 namespace Frost
 {
 	public abstract class Device2D
 	{
 		private readonly DeviceCounterCollection _CounterCollection;
+		private readonly EffectCollection _EffectCollection;
 
 		protected Device2D()
 		{
 			this._CounterCollection = new DeviceCounterCollection();
+			this._EffectCollection = new EffectCollection();
 		}
 
 		public DeviceCounterCollection Diagnostics
 		{
 			get { return this._CounterCollection; }
+		}
+
+		public EffectCollection Effects
+		{
+			get { return this._EffectCollection; }
 		}
 	}
 }
---
+++ Frost/Effects/EffectCollection.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) 2012, Joshua Burke
+// All rights reserved.
+// 
+// See LICENSE for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
+
+namespace Frost.Effects
+{
+	public sealed class EffectCollection
+	{
+		private readonly Dictionary<Type, IEffect> _Effects;
+
+		internal EffectCollection()
+		{
+			this._Effects = new Dictionary<Type, IEffect>();
+		}
+
+		public void Register<T>(Effect<T> effect)
+			where T : struct, IEffectSettings, IEquatable<T>
+		{
+			Contract.Requires(effect != null);
+
+			Trace.Assert(effect != null);
+
+			lock(this._Effects)
+			{
+				this._Effects[typeof(T)] = effect;
+			}
+		}
+
+		public Effect<T> Find<T>()
+			where T : struct, IEffectSettings, IEquatable<T>
+		{
+			IEffect result;
+
+			lock(this._Effects)
+			{
+				this._Effects.TryGetValue(typeof(T), out result);
+			}
+
+			return result as Effect<T>;
+		}
+
+		public void Unregister<T>()
+		{
+			lock(this._Effects)
+			{
+				this._Effects.Remove(typeof(T));
+			}
+		}
+	}
+}
