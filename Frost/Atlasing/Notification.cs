0398d0938cdcde9ead9149484a028566fc6d8475
Josh Burke <fealty@earthlink.net>
2012-02-17 23:42:18 -0800

    atlas notification

---
+++ Frost/Atlasing/Notification.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+using System.Diagnostics.Contracts;
+
+namespace Frost.Atlasing
+{
+	public sealed class Notification
+	{
+		private readonly ISurfaceAtlas _Atlas;
+
+		private volatile bool _Value;
+
+		public Notification(ISurfaceAtlas atlas)
+		{
+			Contract.Requires(atlas != null);
+
+			Trace.Assert(atlas != null);
+
+			this._Value = true;
+			this._Atlas = atlas;
+		}
+
+		public void Invalidate()
+		{
+			this._Value = false;
+		}
+
+		public ISurfaceAtlas Atlas
+		{
+			get
+			{
+				Contract.Ensures(Contract.Result<ISurfaceAtlas>() != null);
+				
+				return this._Atlas;
+			}
+		}
+
+		public bool Value
+		{
+			get { return this._Value; }
+		}
+	}
+}
