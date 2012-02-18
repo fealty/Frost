a2f755534ffc53e4e4d5894ae4471fd7c0b49f02
Josh Burke <fealty@earthlink.net>
2012-02-17 23:58:26 -0800

    device diagnostic counters

---
+++ Frost/Diagnostics/IDeviceCounter.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+
+namespace Frost.Diagnostics
+{
+	namespace Contracts
+	{
+		[ContractClassFor(typeof(IDeviceCounter))]
+		internal abstract class IDeviceCounterContract : IDeviceCounter
+		{
+			public string Name
+			{
+				get
+				{
+					Contract.Ensures(!String.IsNullOrWhiteSpace(Contract.Result<string>()));
+
+					throw new NotSupportedException();
+				}
+			}
+
+			public string Category
+			{
+				get
+				{
+					Contract.Ensures(!String.IsNullOrWhiteSpace(Contract.Result<string>()));
+
+					throw new NotSupportedException();
+				}
+			}
+
+			public object Value
+			{
+				get
+				{
+					Contract.Ensures(Contract.Result<object>() != null);
+
+					throw new NotSupportedException();
+				}
+			}
+
+			public object Average
+			{
+				get
+				{
+					Contract.Ensures(Contract.Result<object>() != null);
+
+					throw new NotSupportedException();
+				}
+			}
+
+			public object Minimum
+			{
+				get
+				{
+					Contract.Ensures(Contract.Result<object>() != null);
+
+					throw new NotSupportedException();
+				}
+			}
+
+			public object Maximum
+			{
+				get
+				{
+					Contract.Ensures(Contract.Result<object>() != null);
+
+					throw new NotSupportedException();
+				}
+			}
+		}
+	}
+
+	[ContractClass(typeof(Contracts.IDeviceCounterContract))]
+	public interface IDeviceCounter
+	{
+		string Name { get; }
+		string Category { get; }
+
+		object Value { get; }
+		object Average { get; }
+		object Minimum { get; }
+		object Maximum { get; }
+	}
+
+	public interface IDeviceCounter<T> : IDeviceCounter
+	{
+		new T Value { get; }
+		new T Average { get; }
+		new T Minimum { get; }
+		new T Maximum { get; }
+	}
+}
---
+++ Frost/Diagnostics/IDeviceCounterCollection.cs
@@ -0,0 +1,68 @@
+﻿// Copyright (c) 2012, Joshua Burke
+// All rights reserved.
+// 
+// See LICENSE for more information.
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+
+namespace Frost.Diagnostics
+{
+	namespace Contracts
+	{
+		[ContractClassFor(typeof(IDeviceCounterCollection))]
+		internal abstract class IDeviceCounterCollectionContract
+			: IDeviceCounterCollection
+		{
+			public abstract
+				IEnumerator
+					<KeyValuePair<KeyValuePair<string, string>, IDeviceCounter>>
+				GetEnumerator();
+
+			IEnumerator IEnumerable.GetEnumerator()
+			{
+				return this.GetEnumerator();
+			}
+
+			public bool Query(
+				string category, string name, out IDeviceCounter result)
+			{
+				Contract.Requires(!string.IsNullOrWhiteSpace(category));
+				Contract.Requires(!string.IsNullOrWhiteSpace(name));
+
+				throw new NotSupportedException();
+			}
+
+			public bool Query<T>(
+				string category, string name, out IDeviceCounter<T> result)
+				where T : struct
+			{
+				Contract.Requires(!string.IsNullOrWhiteSpace(category));
+				Contract.Requires(!string.IsNullOrWhiteSpace(name));
+
+				throw new NotSupportedException();
+			}
+
+			public void Register(IDeviceCounter counter)
+			{
+				Contract.Requires(counter != null);
+			}
+		}
+	}
+
+	[ContractClass(typeof(Contracts.IDeviceCounterCollectionContract))]
+	public interface IDeviceCounterCollection
+		: IEnumerable
+		  	<KeyValuePair<KeyValuePair<string, string>, IDeviceCounter>>
+	{
+		bool Query(string category, string name, out IDeviceCounter result);
+
+		bool Query<T>(
+			string category, string name, out IDeviceCounter<T> result)
+			where T : struct;
+
+		void Register(IDeviceCounter counter);
+	}
+}
