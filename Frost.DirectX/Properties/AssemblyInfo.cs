4ec3799451cc7e5101c400cf38815da768627776
Josh Burke <fealty@earthlink.net>
2012-02-20 01:41:48 -0800

    info

--- Frost.DirectX/Properties/AssemblyInfo.cs
+++ Frost.DirectX/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Frost.DirectX")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A DirectX implementation of Frost.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Frost.DirectX")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
+[assembly: AssemblyCopyright("Copyright © Joshua Burke 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
