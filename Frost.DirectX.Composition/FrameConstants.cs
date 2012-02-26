06e856585f128d1bf1689e54cd96e76e90689c38
Josh Burke <fealty@earthlink.net>
2012-02-26 02:19:40 -0800

    .

---
+++ Frost.DirectX.Composition/FrameConstants.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) 2012, Joshua Burke
+// All rights reserved.
+// 
+// See LICENSE for more information.
+
+using System.Runtime.InteropServices;
+
+using SharpDX;
+
+namespace Frost.DirectX.Composition
+{
+	[StructLayout(LayoutKind.Sequential)] internal struct FrameConstants
+	{
+		public static readonly int ByteSize;
+
+		static FrameConstants()
+		{
+			ByteSize = GPUData.SizeOf<FrameConstants>();
+		}
+
+		public Vector2 ViewportSize;
+		public Vector2 Reserved;
+		public Matrix ViewTransform;
+	}
+}
