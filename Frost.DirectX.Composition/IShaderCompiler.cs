b2ba5210b30538f2dca7bc9f00e9988407290958
Josh Burke <fealty@earthlink.net>
2012-02-26 02:19:02 -0800

    .

---
+++ Frost.DirectX.Composition/IShaderCompiler.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) 2012, Joshua Burke
+// All rights reserved.
+// 
+// See LICENSE for more information.
+
+using System.Diagnostics.Contracts;
+
+using Frost.DirectX.Composition.Contracts;
+
+namespace Frost.DirectX.Composition
+{
+	namespace Contracts
+	{
+		[ContractClassFor(typeof(IShaderCompiler))] internal abstract class IShaderCompilerContract
+			: IShaderCompiler
+		{
+			public void Compile(string text, string function, ref ShaderHandle result)
+			{
+				Contract.Requires(!string.IsNullOrEmpty(text));
+				Contract.Requires(!string.IsNullOrEmpty(function));
+			}
+		}
+	}
+
+	[ContractClass(typeof(IShaderCompilerContract))] public interface IShaderCompiler
+	{
+		void Compile(string text, string function, ref ShaderHandle result);
+	}
+}
