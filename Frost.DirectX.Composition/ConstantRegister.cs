d5c3062e90eeed67fb364ce03ebc06faed832f5a
Josh Burke <fealty@earthlink.net>
2012-02-26 02:19:57 -0800

    .

---
+++ Frost.DirectX.Composition/ConstantRegister.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) 2012, Joshua Burke
+// All rights reserved.
+// 
+// See LICENSE for more information.
+
+namespace Frost.DirectX.Composition
+{
+	public enum ConstantRegister
+	{
+		One = 0,
+		Two = 1,
+		Three = 2,
+		Four = 3,
+		Five = 4,
+		Predefined = 5,
+		Reserved = 6
+	}
+}
