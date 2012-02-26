c35728d679b0348fdbcad06b853a4df04f1dcef3
Josh Burke <fealty@earthlink.net>
2012-02-26 02:18:11 -0800

    .

---
+++ Frost.DirectX.Composition/RenderConstants.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) 2012, Joshua Burke
+// All rights reserved.
+// 
+// See LICENSE for more information.
+
+using System.Runtime.InteropServices;
+
+using SharpDX;
+
+namespace Frost.DirectX.Composition
+{
+	[StructLayout(LayoutKind.Sequential)] internal struct RenderConstants
+	{
+		public static readonly int ByteSize;
+
+		static RenderConstants()
+		{
+			ByteSize = GPUData.SizeOf<RenderConstants>();
+		}
+
+		public Vector4 TextureRegion;
+		public Vector4 DrawnRegion;
+		public Matrix Transform;
+	}
+}
