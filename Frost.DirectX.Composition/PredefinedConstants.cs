4b4defdc03a08188cd239f4feeea307a8db31c18
Josh Burke <fealty@earthlink.net>
2012-02-26 02:18:22 -0800

    .

---
+++ Frost.DirectX.Composition/PredefinedConstants.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) 2012, Joshua Burke
+// All rights reserved.
+// 
+// See LICENSE for more information.
+
+using System.Runtime.InteropServices;
+
+using SharpDX;
+
+namespace Frost.DirectX.Composition
+{
+	[StructLayout(LayoutKind.Sequential)] internal struct PredefinedConstants : IConstantBufferData
+	{
+		public const string ShaderText =
+			@"
+			cbuffer PredefinedConstants : register(b5)
+			{
+				float2 cx_TargetSize : packoffset(c0);
+				float2 cx_TargetTexelSize : packoffset(c0.z);				
+			};
+		";
+
+		public float SizeX;
+		public float SizeY;
+		public float TexelSizeX;
+		public float TexelSizeY;
+
+		public int ByteSize
+		{
+			get { return GPUData.SizeOf<PredefinedConstants>(); }
+		}
+
+		public void Serialize(DataStream stream)
+		{
+			stream.Write(SizeX);
+			stream.Write(SizeY);
+
+			stream.Write(TexelSizeX);
+			stream.Write(TexelSizeY);
+		}
+	}
+}
