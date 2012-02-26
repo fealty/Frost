40c8ed8b3d337f62cb35083c46f65b3d1fb76fd2
Josh Burke <fealty@earthlink.net>
2012-02-26 02:19:29 -0800

    .

---
+++ Frost.DirectX.Composition/GPUData.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) 2012, Joshua Burke
+// All rights reserved.
+// 
+// See LICENSE for more information.
+
+using System;
+using System.Diagnostics.Contracts;
+using System.Linq.Expressions;
+using System.Runtime.InteropServices;
+
+namespace Frost.DirectX.Composition
+{
+	public static class GPUData
+	{
+		public static int OffsetOf<T>(Expression<Func<T, object>> expression) where T : struct
+		{
+			Contract.Requires(expression.NodeType == ExpressionType.Lambda);
+			Contract.Requires(expression.Body is UnaryExpression);
+
+			UnaryExpression unaExp = (UnaryExpression)expression.Body;
+
+			MemberExpression memExp = (MemberExpression)unaExp.Operand;
+
+			return (int)Marshal.OffsetOf(typeof(T), memExp.Member.Name);
+		}
+
+		public static int SizeOf<T>() where T : struct
+		{
+			return Marshal.SizeOf(typeof(T));
+		}
+	}
+}
