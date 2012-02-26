8c30da0a53caa187e5c24eacf91cfd32422917ac
Josh Burke <fealty@earthlink.net>
2012-02-26 07:51:02 -0800

    .

--- Frost.DirectX.Formatting/AnalysisException.cs
+++ Frost.DirectX.Formatting/AnalysisException.cs
@@ -1,11 +1,15 @@
-﻿using System;
+﻿// Copyright (c) 2012, Joshua Burke
+// All rights reserved.
+// 
+// See LICENSE for more information.
+
+using System;
 
 namespace Frost.DirectX.Formatting
 {
 	internal sealed class AnalysisException : Exception
 	{
-		public AnalysisException(Exception exception)
-			: base("Text analysis failed!", exception)
+		public AnalysisException(Exception exception) : base("Text analysis failed!", exception)
 		{
 		}
 	}
