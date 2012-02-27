9a6bec4abfa0f4667b439bbde3c20dc56cdd9266
Josh Burke <fealty@earthlink.net>
2012-02-26 22:27:53 -0800

    .

--- Frost.DirectX.Formatting/ILineProvider.cs
+++ Frost.DirectX.Formatting/ILineProvider.cs
@@ -1,9 +1,31 @@
-﻿using System.Diagnostics.Contracts;
+﻿// Copyright (c) 2012, Joshua Burke
+// All rights reserved.
+// 
+// See LICENSE for more information.
 
-namespace Cabbage.Formatting
+using System;
+using System.Diagnostics.Contracts;
+
+using Frost.DirectX.Formatting.Contracts;
+
+namespace Frost.DirectX.Formatting
 {
-	[ContractClass(typeof(Contracts.ILineProviderContract))]
-	public interface ILineProvider
+	namespace Contracts
+	{
+		[ContractClassFor(typeof(ILineProvider))] internal abstract class ILineProviderContract
+			: ILineProvider
+		{
+			public double ProduceLine(int lineIndex)
+			{
+				Contract.Requires(lineIndex >= 0);
+				Contract.Ensures(Check.IsPositive(Contract.Result<double>()));
+
+				throw new NotSupportedException();
+			}
+		}
+	}
+
+	[ContractClass(typeof(ILineProviderContract))] public interface ILineProvider
 	{
 		double ProduceLine(int lineIndex);
 	}
