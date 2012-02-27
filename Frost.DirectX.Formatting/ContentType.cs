258d5388870ec68326fbe11151405ed26cc050dd
Josh Burke <fealty@earthlink.net>
2012-02-26 21:25:59 -0800

    .

--- Frost.DirectX.Formatting/ContentType.cs
+++ Frost.DirectX.Formatting/ContentType.cs
@@ -1,4 +1,9 @@
-﻿namespace Frost.DirectX.Formatting
+﻿// Copyright (c) 2012, Joshua Burke
+// All rights reserved.
+// 
+// See LICENSE for more information.
+
+namespace Frost.DirectX.Formatting
 {
 	internal enum ContentType
 	{
