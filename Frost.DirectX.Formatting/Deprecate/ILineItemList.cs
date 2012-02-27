acefc2dd7d38da30d5b1eea0a5b086936f5dd0ea
Josh Burke <fealty@earthlink.net>
2012-02-26 22:27:41 -0800

    .

---
+++ Frost.DirectX.Formatting/Deprecate/ILineItemList.cs
@@ -0,0 +1,65 @@
+﻿// Copyright (c) 2012, Joshua Burke
+// All rights reserved.
+// 
+// See LICENSE for more information.
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+
+namespace Frost.DirectX.Formatting
+{
+	namespace Contracts
+	{
+		[ContractClassFor(typeof(ILineItemList))]
+		internal abstract class ILineItemListContract
+			: ILineItemList
+		{
+			public int Count
+			{
+				get
+				{
+					Contract.Ensures(Contract.Result<int>() >= 0);
+
+					throw new NotSupportedException();
+				}
+			}
+
+			public LineItem this[int index]
+			{
+				get
+				{
+					Contract.Requires(index >= 0);
+
+					throw new NotSupportedException();
+				}
+			}
+
+			List<LineItem>.Enumerator ILineItemList.GetEnumerator()
+			{
+				throw new NotSupportedException();
+			}
+
+			IEnumerator<LineItem> IEnumerable<LineItem>.GetEnumerator()
+			{
+				throw new NotSupportedException();
+			}
+
+			IEnumerator IEnumerable.GetEnumerator()
+			{
+				throw new NotSupportedException();
+			}
+		}
+	}
+
+	[ContractClass(typeof(Contracts.ILineItemListContract))] public interface ILineItemList
+		: IEnumerable<LineItem>
+	{
+		int Count { get; }
+
+		LineItem this[int index] { get; }
+
+		new List<LineItem>.Enumerator GetEnumerator();
+	}
+}
