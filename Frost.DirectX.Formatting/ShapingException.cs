0f738d9b5025750440d5674542827399eec4bf23
Josh Burke <fealty@earthlink.net>
2012-02-26 22:49:52 -0800

    .

--- Frost.DirectX.Formatting/ShapingException.cs
+++ Frost.DirectX.Formatting/ShapingException.cs
@@ -1,11 +1,15 @@
-﻿using System;
+﻿// Copyright (c) 2012, Joshua Burke
+// All rights reserved.
+// 
+// See LICENSE for more information.
+
+using System;
 
 namespace Frost.DirectX.Formatting
 {
 	internal sealed class ShapingException : Exception
 	{
-		public ShapingException(Exception exception)
-			: base("Text shaping failed!", exception)
+		public ShapingException(Exception exception) : base("Text shaping failed!", exception)
 		{
 		}
 	}
