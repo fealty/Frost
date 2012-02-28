97d7d8b33d5d57ea2cc3e50a90dac52b3523ca4e
Josh Burke <fealty@earthlink.net>
2012-02-28 07:23:14 -0800

    contracts

--- Demo.Framework/IDemoContext.cs
+++ Demo.Framework/IDemoContext.cs
@@ -4,12 +4,40 @@
 // See LICENSE for more information.
 
 using System;
+using System.Diagnostics.Contracts;
+
+using Demo.Framework.Contracts;
 
 using Frost;
 
 namespace Demo.Framework
 {
-	public interface IDemoContext : IDisposable
+	namespace Contracts
+	{
+		[ContractClassFor(typeof(IDemoContext))] internal abstract class IDemoContextContract
+			: IDemoContext
+		{
+			public abstract void Dispose();
+
+			public string Name
+			{
+				get
+				{
+					Contract.Ensures(Contract.Result<string>() != null);
+
+					throw new NotSupportedException();
+				}
+			}
+
+			public void Reset(Canvas target, Device2D device2D)
+			{
+				Contract.Requires(device2D != null);
+				Contract.Requires(Check.IsValid(target, device2D));
+			}
+		}
+	}
+
+	[ContractClass(typeof(IDemoContextContract))] public interface IDemoContext : IDisposable
 	{
 		string Name { get; }
 
