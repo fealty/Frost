b87d9238e538ede0301cc6d1aee950f32701cfd5
Josh Burke <fealty@earthlink.net>
2012-03-02 12:05:16 -0800

    cleanup/docs

--- Frost.DirectX.Formatting/Formatter.cs
+++ Frost.DirectX.Formatting/Formatter.cs
@@ -14,12 +14,19 @@ using DxFontMetrics = SharpDX.DirectWrite.FontMetrics;
 
 namespace Frost.DirectX.Formatting
 {
+	/// <summary>
+	///   This class formats typeset clusters and glyphs, producing formatting information and outputting results to an output sink.
+	/// </summary>
 	internal sealed class Formatter
 	{
 		private readonly FormatterSink _OutputSink;
 
 		private FormattedCluster[] _Clusters;
 
+		/// <summary>
+		///   This constructor links a new instance of this class to an output sink.
+		/// </summary>
+		/// <param name="outputSink"> This parameter references the output sink. </param>
 		public Formatter(FormatterSink outputSink)
 		{
 			Contract.Requires(outputSink != null);
@@ -27,6 +34,10 @@ namespace Frost.DirectX.Formatting
 			_OutputSink = outputSink;
 		}
 
+		/// <summary>
+		///   This method formats the input clusters and glyphs.
+		/// </summary>
+		/// <param name="input"> This parameter references the input sink. </param>
 		public void Format(TypesetterSink input)
 		{
 			Contract.Requires(input != null);
@@ -46,12 +57,16 @@ namespace Frost.DirectX.Formatting
 			_OutputSink.PreallocateClusters(input.Clusters.Count);
 
 			OutputGlyphs(input);
-
 			ProduceLines(input);
 
 			DetermineBaselines();
 		}
 
+		/// <summary>
+		///   This method produces the final formatted cluster runs.
+		/// </summary>
+		/// <param name="input"> This parameter references the input sink. </param>
+		/// <returns> This method returns an enumeration of the results. </returns>
 		private static IEnumerable<FormattedRun> ProduceFinalRuns(TypesetterSink input)
 		{
 			Contract.Requires(input != null);
@@ -60,7 +75,7 @@ namespace Frost.DirectX.Formatting
 
 			activeRun.BidiLevel = input.Clusters[0].BidiLevel;
 			activeRun.Clusters = IndexedRange.Empty;
-			activeRun.EmSize = Convert.ToSingle(input.Clusters[0].Advance.Height);
+			activeRun.EmSize = input.Clusters[0].Advance.Height;
 			activeRun.Font = input.Clusters[0].Font;
 			activeRun.TextRange = IndexedRange.Empty;
 			activeRun.PointSize = input.Clusters[0].PointSize;
@@ -75,12 +90,13 @@ namespace Frost.DirectX.Formatting
 
 				currentRun.BidiLevel = input.Clusters[i].BidiLevel;
 				currentRun.Clusters = activeRun.Clusters;
-				currentRun.EmSize = Convert.ToSingle(input.Clusters[i].Advance.Height);
+				currentRun.EmSize = input.Clusters[i].Advance.Height;
 				currentRun.Font = input.Clusters[i].Font;
 				currentRun.TextRange = activeRun.TextRange;
 				currentRun.PointSize = input.Clusters[i].PointSize;
 				currentRun.LineNumber = input.Clusters[i].LineNumber;
 
+				// the runs are identical
 				if(currentRun == activeRun)
 				{
 					// extend the current cluster range by one cluster
@@ -88,9 +104,11 @@ namespace Frost.DirectX.Formatting
 				}
 				else
 				{
+					// determine the text range from the clusters
 					start = input.Clusters[activeRun.Clusters.StartIndex].Characters.StartIndex;
 					end = input.Clusters[activeRun.Clusters.LastIndex].Characters.LastIndex;
 
+					// convert the valid index range to a length
 					activeRun.TextRange = new IndexedRange(start, (end - start) + 1);
 
 					yield return activeRun;
@@ -101,14 +119,21 @@ namespace Frost.DirectX.Formatting
 				}
 			}
 
+			// determine the text range from the clusters
 			start = input.Clusters[activeRun.Clusters.StartIndex].Characters.StartIndex;
 			end = input.Clusters[activeRun.Clusters.LastIndex].Characters.LastIndex;
 
+			// convert the valid index range to a length
 			activeRun.TextRange = new IndexedRange(start, (end - start) + 1);
 
 			yield return activeRun;
 		}
 
+		/// <summary>
+		///   This method produces the bidi formatted cluster runs.
+		/// </summary>
+		/// <param name="input"> This parameter references the input sink. </param>
+		/// <returns> This method returns an enumeration of the results. </returns>
 		private static IEnumerable<FormattedRun> ProduceBidiRuns(TypesetterSink input)
 		{
 			Contract.Requires(input != null);
@@ -117,7 +142,7 @@ namespace Frost.DirectX.Formatting
 
 			activeRun.BidiLevel = input.Clusters[0].BidiLevel;
 			activeRun.Clusters = IndexedRange.Empty;
-			activeRun.EmSize = Convert.ToSingle(input.Clusters[0].Advance.Height);
+			activeRun.EmSize = input.Clusters[0].Advance.Height;
 			activeRun.Font = input.Clusters[0].Font;
 			activeRun.PointSize = input.Clusters[0].PointSize;
 			activeRun.TextRange = IndexedRange.Empty;
@@ -153,6 +178,10 @@ namespace Frost.DirectX.Formatting
 			yield return activeRun;
 		}
 
+		/// <summary>
+		///   This method produces formatted lines of clusters.
+		/// </summary>
+		/// <param name="input"> This parameter references the input sink. </param>
 		private void ProduceLines(TypesetterSink input)
 		{
 			Contract.Requires(input != null);
@@ -166,43 +195,65 @@ namespace Frost.DirectX.Formatting
 			{
 				if(run.LineNumber != lineNumber)
 				{
-					if(lineNumber == -1)
+					lineNumber = run.LineNumber;
+
+					if(lineNumber == 0)
 					{
-						// the line bidi level is determined by the first run
+						// the first run determines the line bidi level
 						lineBidiLevel = run.BidiLevel;
 
 						if(input.Alignment == Alignment.Leading)
 						{
-							// reverse the bidi level of the line for leading alignment
+							// reverse the bidi level for leading alignment
 							lineBidiLevel++;
 						}
 					}
 
-					lineNumber = run.LineNumber;
+					bool isLeftToRight = lineBidiLevel % 2 == 0;
 
 					if(input.Alignment != Alignment.Center)
 					{
+						penPosition = 0.0f;
+
+						if(!isLeftToRight)
+						{
+							// position relative to the right edge
+							penPosition = input.Lines[lineNumber].Right;
+						}
+
+						// indent only the first line in the paragraph
 						if(lineNumber == 0)
 						{
-							// indent only the first line
-							penPosition = lineBidiLevel % 2 == 0
-							              	? input.Indentation
-							              	: input.Lines[lineNumber].Right - input.Indentation;
-						}
-						else
-						{
-							penPosition = lineBidiLevel % 2 == 0 ? 0.0f : input.Lines[lineNumber].Right;
+							if(isLeftToRight)
+							{
+								// position relative to the left edge
+								penPosition = input.Indentation;
+							}
+							else
+							{
+								// position relative to the right edge
+								penPosition = penPosition - input.Indentation;
+							}
 						}
 					}
 					else
 					{
 						float lineWidth = input.Lines[lineNumber].Width;
 
+						// determine unused space on the line
 						lineWidth -= input.LineLengths[lineNumber];
 
+						// divide the unused space in half
 						lineWidth /= 2.0f;
 
-						penPosition = lineBidiLevel % 2 == 0 ? lineWidth : input.Lines[lineNumber].Right - lineWidth;
+						// distribute the halved space to center the line
+						penPosition = lineWidth;
+
+						if(!isLeftToRight)
+						{
+							// position relative to the right edge
+							penPosition = input.Lines[lineNumber].Right - penPosition;
+						}
 					}
 				}
 
@@ -214,6 +265,10 @@ namespace Frost.DirectX.Formatting
 			_OutputSink.Runs.AddRange(ProduceFinalRuns(input));
 		}
 
+		/// <summary>
+		///   This method produces the formatted glyphs.
+		/// </summary>
+		/// <param name="input"> This parameter references the input sink. </param>
 		private void OutputGlyphs(TypesetterSink input)
 		{
 			Contract.Requires(input != null);
@@ -230,6 +285,13 @@ namespace Frost.DirectX.Formatting
 			}
 		}
 
+		/// <summary>
+		///   This method produces the formatted clusters for each line from each run.
+		/// </summary>
+		/// <param name="run"> This parameter contains the formatted run. </param>
+		/// <param name="lineBidiLevel"> This parameter indicates the bidi level of the line. </param>
+		/// <param name="penPosition"> This parameter indicates the pen position. </param>
+		/// <param name="input"> This parameter references the input sink. </param>
 		private void ProduceClusters(
 			FormattedRun run, int lineBidiLevel, ref float penPosition, TypesetterSink input)
 		{
@@ -238,37 +300,42 @@ namespace Frost.DirectX.Formatting
 			// the run and the line have the same direction
 			if((run.BidiLevel % 2 == 0) == (lineBidiLevel % 2 == 0))
 			{
-				for(int i = run.Clusters.StartIndex; i <= run.Clusters.LastIndex; ++i)
+				foreach(int clusterIndex in run.Clusters)
 				{
 					FormattedCluster newCluster;
 
 					newCluster.Region = input.Lines[run.LineNumber];
 
-					TypesetCluster cluster = input.Clusters[i];
+					TypesetCluster cluster = input.Clusters[clusterIndex];
 
 					newCluster.Characters = cluster.Characters;
 					newCluster.Glyphs = cluster.Glyphs;
 					newCluster.Display = cluster.Display;
 
-					newCluster.Region = new Rectangle(
-						newCluster.Region.Location, new Size(cluster.Advance.Width, newCluster.Region.Height));
+					float clusterWidth = cluster.Advance.Width;
+					float clusterHeight = newCluster.Region.Height;
+
+					Size clusterSize = new Size(clusterWidth, clusterHeight);
+
+					newCluster.Region = newCluster.Region.Resize(clusterSize);
 
 					// the line reads from left to right
 					if(lineBidiLevel % 2 == 0)
 					{
-						newCluster.Region =
-							new Rectangle(
-								new Point(newCluster.Region.X + penPosition, newCluster.Region.Y), newCluster.Region.Size);
+						Size amount = new Size(penPosition, 0.0f);
+
+						newCluster.Region = newCluster.Region.Translate(amount);
 
 						penPosition += newCluster.Region.Width;
 					}
 						// the line reads from right to left
 					else
 					{
-						newCluster.Region =
-							new Rectangle(
-								new Point(penPosition - newCluster.Region.Width, newCluster.Region.Y),
-								newCluster.Region.Size);
+						float x = penPosition - newCluster.Region.Width;
+
+						Point location = new Point(x, newCluster.Region.Y);
+
+						newCluster.Region = newCluster.Region.Relocate(location);
 
 						penPosition -= newCluster.Region.Width;
 					}
@@ -278,7 +345,7 @@ namespace Frost.DirectX.Formatting
 						newCluster.Region = cluster.Floater;
 					}
 
-					_Clusters[i] = newCluster;
+					_Clusters[clusterIndex] = newCluster;
 				}
 			}
 			else
@@ -296,26 +363,30 @@ namespace Frost.DirectX.Formatting
 					newCluster.Glyphs = cluster.Glyphs;
 					newCluster.Display = cluster.Display;
 
-					newCluster.Region = new Rectangle(
-						newCluster.Region.Location, new Size(cluster.Advance.Width, newCluster.Region.Height));
+					float clusterWidth = cluster.Advance.Width;
+					float clusterHeight = newCluster.Region.Height;
+
+					Size clusterSize = new Size(clusterWidth, clusterHeight);
+
+					newCluster.Region = newCluster.Region.Resize(clusterSize);
 
 					// the line reads from left to right
 					if(lineBidiLevel % 2 == 0)
 					{
-						newCluster.Region =
-							new Rectangle(
-								new Point(newCluster.Region.X + penPosition, newCluster.Region.Y), newCluster.Region.Size);
+						Size amount = new Size(penPosition, 0.0f);
+
+						newCluster.Region = newCluster.Region.Translate(amount);
 
 						penPosition += newCluster.Region.Width;
 					}
-
 						// the line reads from right to left
 					else
 					{
-						newCluster.Region =
-							new Rectangle(
-								new Point(penPosition - newCluster.Region.Width, newCluster.Region.Y),
-								newCluster.Region.Size);
+						float x = penPosition - newCluster.Region.Width;
+
+						Point location = new Point(x, newCluster.Region.Y);
+
+						newCluster.Region = newCluster.Region.Relocate(location);
 
 						penPosition -= newCluster.Region.Width;
 					}
@@ -330,6 +401,12 @@ namespace Frost.DirectX.Formatting
 			}
 		}
 
+		/// <summary>
+		///   This method determines the max of two absolute values.
+		/// </summary>
+		/// <param name="n1"> This parameter contains the first value to test. </param>
+		/// <param name="n2"> This parameter contains the second value to test. </param>
+		/// <returns> This method returns the max absolute value. </returns>
 		private static float TakeAbsMax(float n1, float n2)
 		{
 			double n1Abs = Math.Abs(n1);
@@ -338,12 +415,14 @@ namespace Frost.DirectX.Formatting
 			return n1Abs > n2Abs ? n1 : n2;
 		}
 
+		/// <summary>
+		///   This method determines the common baselines for the paragraph.
+		/// </summary>
 		private void DetermineBaselines()
 		{
 			float baselineOffset = 0.0f;
 			float strikethroughOffset = 0.0f;
 			float underlineOffset = 0.0f;
-
 			float strikethroughThickness = 0.0f;
 			float underlineThickness = 0.0f;
 
