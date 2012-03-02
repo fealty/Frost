93fafeaa9f40361cf116c6dfe0c73c68fa80c795
Josh Burke <fealty@earthlink.net>
2012-03-02 12:33:27 -0800

    cleanup/docs

--- Frost.DirectX.Formatting/IndexingHelpers.cs
+++ Frost.DirectX.Formatting/IndexingHelpers.cs
@@ -7,8 +7,19 @@ using System.Collections.Generic;
 
 namespace Frost.DirectX.Formatting
 {
+	/// <summary>
+	///   This class provides convenient extension methods.
+	/// </summary>
 	internal static class IndexingHelpers
 	{
+		/// <summary>
+		///   This method tries to get the next item in a list.
+		/// </summary>
+		/// <typeparam name="T"> This type parameter indicates the type of item. </typeparam>
+		/// <param name="this"> This parameter references a <see cref="List{T}" /> instance. </param>
+		/// <param name="currentIndex"> This parameter indicates the current index. </param>
+		/// <param name="item"> This output parameter contains the next item. </param>
+		/// <returns> This method returns <c>true</c> if a next item exists; otherwise, this method returns <c>false</c> . </returns>
 		public static bool TryGetNextItem<T>(this List<T> @this, int currentIndex, out T item)
 		{
 			if(currentIndex >= -1 && currentIndex + 1 < @this.Count)
@@ -23,6 +34,14 @@ namespace Frost.DirectX.Formatting
 			return false;
 		}
 
+		/// <summary>
+		///   This method tries to get the previous item in a list.
+		/// </summary>
+		/// <typeparam name="T"> This type parameter indicates the type of item. </typeparam>
+		/// <param name="this"> This parameter references a <see cref="List{T}" /> instance. </param>
+		/// <param name="currentIndex"> This parameter indicates the current index. </param>
+		/// <param name="item"> This output parameter contains the previous item. </param>
+		/// <returns> This method returns <c>true</c> if a previous item exists; otherwise, this method returns <c>false</c> . </returns>
 		public static bool TryGetPreviousItem<T>(this List<T> @this, int currentIndex, out T item)
 		{
 			if(currentIndex >= 1 && currentIndex - 1 < @this.Count)
@@ -37,6 +56,14 @@ namespace Frost.DirectX.Formatting
 			return false;
 		}
 
+		/// <summary>
+		///   This method tries to get the value of the current index.
+		/// </summary>
+		/// <typeparam name="T"> This type parameter indicates the type of item. </typeparam>
+		/// <param name="this"> This parameter references a <see cref="List{T}" /> instance. </param>
+		/// <param name="currentIndex"> This parameter indicates the current index. </param>
+		/// <param name="item"> This output parameter contains the current item. </param>
+		/// <returns> This method returns <c>true</c> if a current item exists; otherwise, this method returns <c>false</c> . </returns>
 		public static bool TryCurrentOrDefault<T>(this List<T> @this, int currentIndex, out T item)
 		{
 			if(currentIndex >= 0 && currentIndex < @this.Count)
