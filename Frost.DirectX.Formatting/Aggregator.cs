91b93b392e86c0ceac583537b9c5852396e1bec3
Josh Burke <fealty@earthlink.net>
2012-03-02 08:51:35 -0800

    cleanup/docs

--- Frost.DirectX.Formatting/Aggregator.cs
+++ Frost.DirectX.Formatting/Aggregator.cs
@@ -19,10 +19,17 @@ using FontWeight = Frost.Formatting.FontWeight;
 
 namespace Frost.DirectX.Formatting
 {
+	/// <summary>
+	///   This class provides an interface to apply formatting properties to individual ranges of characters.
+	/// </summary>
 	internal sealed class Aggregator : CallbackBase, TextAnalysisSink
 	{
 		private readonly AggregatorSink _OutputSink;
 
+		/// <summary>
+		///   This constructor links a new instance of this class to an output sink.
+		/// </summary>
+		/// <param name="outputSink"> This parameter references the output sink where results are stored. </param>
 		public Aggregator(AggregatorSink outputSink)
 		{
 			Contract.Requires(outputSink != null);
@@ -30,6 +37,12 @@ namespace Frost.DirectX.Formatting
 			_OutputSink = outputSink;
 		}
 
+		/// <summary>
+		///   This method applies the results of script analysis to a range of characters.
+		/// </summary>
+		/// <param name="textPosition"> This parameter indicates the starting index of the range. </param>
+		/// <param name="textLength"> This parameter indicates the length of the range. </param>
+		/// <param name="scriptAnalysis"> This parameter contains the results to apply to the range. </param>
 		void TextAnalysisSink.SetScriptAnalysis(
 			int textPosition, int textLength, ScriptAnalysis scriptAnalysis)
 		{
@@ -41,6 +54,12 @@ namespace Frost.DirectX.Formatting
 			}
 		}
 
+		/// <summary>
+		///   This method applies the results of breakpoint analysis to a range of characters.
+		/// </summary>
+		/// <param name="textPosition"> This parameter indicates the starting index of the range. </param>
+		/// <param name="textLength"> This parameter indicates the length of the range. </param>
+		/// <param name="lineBreakpoints"> This parameter contains the results to apply to the range. </param>
 		void TextAnalysisSink.SetLineBreakpoints(
 			int textPosition, int textLength, DxLineBreakpoint[] lineBreakpoints)
 		{
@@ -53,6 +72,13 @@ namespace Frost.DirectX.Formatting
 			}
 		}
 
+		/// <summary>
+		///   This method applies the results of bidi-text analysis to a range of characters.
+		/// </summary>
+		/// <param name="textPosition"> This parameter indicates the starting index of the range. </param>
+		/// <param name="textLength"> This parameter indicates the length of the range. </param>
+		/// <param name="explicitLevel"> This parameter indicates the explicit bidi level for the range. </param>
+		/// <param name="resolvedLevel"> This parameter indicates the resolved bidi level for the range. </param>
 		void TextAnalysisSink.SetBidiLevel(
 			int textPosition, int textLength, byte explicitLevel, byte resolvedLevel)
 		{
@@ -64,6 +90,12 @@ namespace Frost.DirectX.Formatting
 			}
 		}
 
+		/// <summary>
+		///   This method applies the results of number substition analysis to a range of characters.
+		/// </summary>
+		/// <param name="textPosition"> This parameter indicates the starting index of the range. </param>
+		/// <param name="textLength"> This parameter indicates the length of the range. </param>
+		/// <param name="numberSubstitution"> This parameter references the number substition to apply to the range. </param>
 		void TextAnalysisSink.SetNumberSubstitution(
 			int textPosition, int textLength, NumberSubstitution numberSubstitution)
 		{
@@ -75,6 +107,10 @@ namespace Frost.DirectX.Formatting
 			}
 		}
 
+		/// <summary>
+		///   This method prepares the output sink for aggregation.
+		/// </summary>
+		/// <param name="text"> This parameter references the text applied to by ranges. </param>
 		public void BeginAggregation(string text)
 		{
 			Contract.Requires(!string.IsNullOrEmpty(text));
@@ -83,6 +119,11 @@ namespace Frost.DirectX.Formatting
 			_OutputSink.Capacity = text.Length;
 		}
 
+		/// <summary>
+		///   This method applies the given culture to a range of characters.
+		/// </summary>
+		/// <param name="textRange"> This parameter indicates the range of characters to modify. </param>
+		/// <param name="culture"> This parameter references the culture to apply to the range. </param>
 		public void SetCulture(IndexedRange textRange, CultureInfo culture)
 		{
 			Contract.Assert(textRange.IsWithin(_OutputSink.FullText));
@@ -93,6 +134,11 @@ namespace Frost.DirectX.Formatting
 			}
 		}
 
+		/// <summary>
+		///   This method applies the given font family to a range of characters.
+		/// </summary>
+		/// <param name="textRange"> This parameter indicates the range of characters to modify. </param>
+		/// <param name="family"> This parameter references the font family to apply to the range. </param>
 		public void SetFamily(IndexedRange textRange, string family)
 		{
 			Contract.Assert(textRange.IsWithin(_OutputSink.FullText));
@@ -103,6 +149,11 @@ namespace Frost.DirectX.Formatting
 			}
 		}
 
+		/// <summary>
+		///   This method applies the given font features to a range of characters.
+		/// </summary>
+		/// <param name="textRange"> This parameter indicates the range of characters to modify. </param>
+		/// <param name="features"> This parameter references the collection of font features to apply to the range. </param>
 		public void SetFeatures(IndexedRange textRange, FontFeatureCollection features)
 		{
 			Contract.Assert(textRange.IsWithin(_OutputSink.FullText));
@@ -113,6 +164,13 @@ namespace Frost.DirectX.Formatting
 			}
 		}
 
+		/// <summary>
+		///   This method transforms the characters of the given text range to inline objects.
+		/// </summary>
+		/// <param name="textRange"> This parameter indicates the range of characters to modify. </param>
+		/// <param name="inline"> This parameter indicates the size of the inline object. </param>
+		/// <param name="hAlignment"> This parameter indicates the horizontal alignment of the inline object. </param>
+		/// <param name="vAlignment"> This parameter indicates the vertical alignment of the inline object. </param>
 		public void SetInline(
 			IndexedRange textRange, Size inline, Alignment hAlignment, Alignment vAlignment)
 		{
@@ -126,6 +184,11 @@ namespace Frost.DirectX.Formatting
 			}
 		}
 
+		/// <summary>
+		///   This method applies the given font point size to a range of characters.
+		/// </summary>
+		/// <param name="textRange"> This parameter indicates the range of characters to modify. </param>
+		/// <param name="pointSize"> This parameter contains the font size to apply to the range. </param>
 		public void SetPointSize(IndexedRange textRange, float pointSize)
 		{
 			Contract.Requires(Check.IsPositive(pointSize));
@@ -137,6 +200,11 @@ namespace Frost.DirectX.Formatting
 			}
 		}
 
+		/// <summary>
+		///   This method applies the given font stretch to a range of characters.
+		/// </summary>
+		/// <param name="textRange"> This parameter indicates the range of characters to modify. </param>
+		/// <param name="stretch"> This parameter indicates the font stretch to apply to the range. </param>
 		public void SetStretch(IndexedRange textRange, FontStretch stretch)
 		{
 			Contract.Assert(textRange.IsWithin(_OutputSink.FullText));
@@ -147,6 +215,11 @@ namespace Frost.DirectX.Formatting
 			}
 		}
 
+		/// <summary>
+		///   This method applies the given font style to a range of characters.
+		/// </summary>
+		/// <param name="textRange"> This parameter indicates the range of characters to modify. </param>
+		/// <param name="style"> This parameter indicates the font style to apply to the range. </param>
 		public void SetStyle(IndexedRange textRange, FontStyle style)
 		{
 			Contract.Assert(textRange.IsWithin(_OutputSink.FullText));
@@ -157,6 +230,11 @@ namespace Frost.DirectX.Formatting
 			}
 		}
 
+		/// <summary>
+		///   This method applies the given font weight to a range of characters.
+		/// </summary>
+		/// <param name="textRange"> This parameter indicates the range of characters to modify. </param>
+		/// <param name="weight"> This parameter indicates the font weight to apply to the range. </param>
 		public void SetWeight(IndexedRange textRange, FontWeight weight)
 		{
 			Contract.Assert(textRange.IsWithin(_OutputSink.FullText));
