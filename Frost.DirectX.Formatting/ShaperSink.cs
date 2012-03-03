689ba227dbd36e9e1ec4a7707c83ff5bbb606e98
Josh Burke <fealty@earthlink.net>
2012-03-02 22:43:52 -0800

    cleanup/docs

--- Frost.DirectX.Formatting/ShaperSink.cs
+++ Frost.DirectX.Formatting/ShaperSink.cs
@@ -8,6 +8,9 @@ using System.Diagnostics.Contracts;
 
 namespace Frost.DirectX.Formatting
 {
+	/// <summary>
+	///   This class provides storage for results from an <see cref="Shaper" /> .
+	/// </summary>
 	internal sealed class ShaperSink
 	{
 		private readonly List<ShapedCluster> _Clusters;
@@ -18,30 +21,38 @@ namespace Frost.DirectX.Formatting
 		public ShaperSink()
 		{
 			_Glyphs = new List<ShapedGlyph>();
-
 			_Clusters = new List<ShapedCluster>();
 		}
 
+		/// <summary>
+		///   This property exposes the shaped glyphs.
+		/// </summary>
 		public List<ShapedGlyph> Glyphs
 		{
 			get
 			{
 				Contract.Ensures(Contract.Result<List<ShapedGlyph>>() != null);
-				
+
 				return _Glyphs;
 			}
 		}
 
+		/// <summary>
+		///   This property exposes the shaped clusters.
+		/// </summary>
 		public List<ShapedCluster> Clusters
 		{
 			get
 			{
 				Contract.Ensures(Contract.Result<List<ShapedCluster>>() != null);
-				
+
 				return _Clusters;
 			}
 		}
 
+		/// <summary>
+		///   This property contains the text that was formatted.
+		/// </summary>
 		public string FullText
 		{
 			get
@@ -58,6 +69,10 @@ namespace Frost.DirectX.Formatting
 			}
 		}
 
+		/// <summary>
+		///   This method ensures that the glyph list can hold the given count of glyphs.
+		/// </summary>
+		/// <param name="count"> This parameter indicates how many glyphs the list must hold. </param>
 		public void PreallocateGlyphs(int count)
 		{
 			Contract.Requires(count >= 0);
@@ -68,6 +83,10 @@ namespace Frost.DirectX.Formatting
 			}
 		}
 
+		/// <summary>
+		///   This method ensures that the clusters list can hold given count of clusters.
+		/// </summary>
+		/// <param name="count"> This parameter indicates how many clusters the list must hold. </param>
 		public void PreallocateClusters(int count)
 		{
 			Contract.Requires(count >= 0);
@@ -78,6 +97,10 @@ namespace Frost.DirectX.Formatting
 			}
 		}
 
+		/// <summary>
+		///   This method prepares the sink for shaping output.
+		/// </summary>
+		/// <param name="fullText"> This parameter references the text to be shaped. </param>
 		public void Reset(string fullText)
 		{
 			Contract.Requires(!string.IsNullOrEmpty(fullText));
