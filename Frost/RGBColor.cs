9c48a13115c552f36508441fe1c7a5ff02b0d7f3
Josh Burke <fealty@earthlink.net>
2012-05-13 04:56:06 -0700

    docs

--- Frost/RGBColor.cs
+++ Frost/RGBColor.cs
@@ -1,13 +1,22 @@
-﻿// Copyright (c) 2012, Joshua Burke
+﻿#region Licensing
+
+// # Licensing Information
+// 
+// Copyright (c) 2012, Joshua Burke  
 // All rights reserved.
 // 
 // See LICENSE for more information.
+
+#endregion
 
 using System;
 using System.Diagnostics.Contracts;
 
 namespace Frost
 {
+	/// <summary>
+	/// represents a color having red, green, blue, and alpha components
+	/// </summary>
 	public struct RGBColor : IEquatable<RGBColor>
 	{
 		private readonly float _A;
@@ -15,7 +24,8 @@ namespace Frost
 		private readonly float _G;
 		private readonly float _R;
 
-		[ContractInvariantMethod] private void Invariant()
+		[ContractInvariantMethod]
+		private void Invariant()
 		{
 			Contract.Invariant(Check.IsByte(_R));
 			Contract.Invariant(Check.IsByte(_G));
@@ -23,6 +33,13 @@ namespace Frost
 			Contract.Invariant(Check.IsPercentage(_A));
 		}
 
+		/// <summary>
+		/// constructs a <see cref="	RGBColor"/> from red, green, blue, and optionally alpha components
+		/// </summary>
+		/// <param name="red">the red value ranging from zero to two hundred fifty-five</param>
+		/// <param name="green">the green value ranging from zero to two hundred fifty-five</param>
+		/// <param name="blue">the blue value ranging from zero to two hundred fifty-five</param>
+		/// <param name="alpha">the alpha value ranging from zero to one hundred</param>
 		public RGBColor(float red, float green, float blue, float alpha = 100.0f)
 		{
 			Contract.Requires(Check.IsByte(red));
@@ -46,6 +63,9 @@ namespace Frost
 			Contract.Assert(A.Equals(alpha));
 		}
 
+		/// <summary>
+		/// gets the red value ranging from zero to two hundred fifty-five
+		/// </summary>
 		public float R
 		{
 			get
@@ -57,6 +77,9 @@ namespace Frost
 			}
 		}
 
+		/// <summary>
+		/// gets the green value ranging from zero to two hundred fifty-five
+		/// </summary>
 		public float G
 		{
 			get
@@ -68,6 +91,9 @@ namespace Frost
 			}
 		}
 
+		/// <summary>
+		/// gets the blue value ranging from zero to two hundred fifty-five
+		/// </summary>
 		public float B
 		{
 			get
@@ -79,6 +105,9 @@ namespace Frost
 			}
 		}
 
+		/// <summary>
+		/// gets the alpha value ranging from zero to one hundred
+		/// </summary>
 		public float A
 		{
 			get
@@ -92,7 +121,8 @@ namespace Frost
 
 		public bool Equals(RGBColor other)
 		{
-			return other._A.Equals(_A) && other._B.Equals(_B) && other._G.Equals(_G) && other._R.Equals(_R);
+			return other._A.Equals(_A) && other._B.Equals(_B) && other._G.Equals(_G) &&
+				other._R.Equals(_R);
 		}
 
 		public override string ToString()
@@ -127,24 +157,42 @@ namespace Frost
 			return new Color(_R / 255.0f, _G / 255.0f, _B / 255.0f, _A / 100.0f);
 		}
 
+		/// <summary>
+		///   implicitly converts a <see cref="Color" /> to a <see cref="RGBColor" />
+		/// </summary>
+		/// <param name="color"> the <see cref="Color" /> to convert </param>
+		/// <returns> the <paramref name="color" /> converted to a <see cref="RGBColor" /> </returns>
 		public static implicit operator RGBColor(Color color)
 		{
-			return new RGBColor(color.R * 255.0f, color.G * 255.0f, color.B * 255.0f, color.A * 100.0f);
+			return new RGBColor(
+				color.R * 255.0f, color.G * 255.0f, color.B * 255.0f, color.A * 100.0f);
 		}
 
+		/// <summary>
+		///   determines whether two instances of <see cref="RGBColor" /> are equal
+		/// </summary>
+		/// <param name="left"> the left operand </param>
+		/// <param name="right"> the right operand </param>
+		/// <returns> <c>true</c> if <paramref name="left" /> equals <paramref name="right" /> ; otherwise, <c>false</c> </returns>
 		public static bool operator ==(RGBColor left, RGBColor right)
 		{
 			return left.Equals(right);
 		}
 
-
+		/// <summary>
+		///   determines whether two instances of <see cref="RGBColor" /> are not equal
+		/// </summary>
+		/// <param name="left"> the left operand </param>
+		/// <param name="right"> the right operand </param>
+		/// <returns> <c>true</c> if <paramref name="left" /> does not equal <paramref name="right" /> ; otherwise, <c>false</c> </returns>
 		public static bool operator !=(RGBColor left, RGBColor right)
 		{
 			return !left.Equals(right);
 		}
 
 #if(UNIT_TESTING)
-		[Fact] internal static void Test0()
+		[Fact]
+		internal static void Test0()
 		{
 			Assert.Equal(1, new RGBColor(1, 2, 3, 4).R);
 			Assert.Equal(2, new RGBColor(1, 2, 3, 4).G);
